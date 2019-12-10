b23ed35ae750008ab5df66b82785c40f58e7242a
yugeshtungala <yugeshtungala342@gmail.com>
2019-12-10 15:22:46 +0530

    push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pract';
+ // price=" 1999";
+  // count=0;
+  // warning="";
+  // clickon(){
+  //   this.count=this.count+1
+  //   this.warning=""
+  // }
+
+//   clickoff(){
+//     if(this.count==0){
+//       this.warning="sorry negative numbers not allowed"
+//      }else{
+    
+//     this.count=this.count-1
+//   }
+// }
+// ng if & for
+
+name="";
+price:number;
+count:number;
+stater=[{
+  name:'chicken' ,price:120,count:1
+},
+{name:'mutton',price:420,count:1}
+];
+
+show(){
+this.stater.push(
+ {name:this.name,
+price:this.price,
+count:this.count}
+);
+this.name=null;
+this.price=null;
+this.count=null;
 }
+del(){
+  this.stater.splice(0,1);
+}
+
+head=['non-veg ','price']
+}
+
+
